b6ece7206648ab9bbc94afcdc2a182b332ce705f
Scott Russell <scottedwardrussell@gmail.com>
2015-03-28 22:48:15 -0400

    created steps 1 and 2 apps

---
+++ training-steps/02_app.js
@@ -0,0 +1,22 @@
+var MongoClient = require('mongodb').MongoClient;
+
+// Open the connection to the server
+MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
+    if(err) throw err;
+
+    // Find one document in our collection
+    db.collection('coll').findOne({}, function(err, doc) {
+
+        if(err) throw err;
+
+        // Print the result
+        console.dir(doc);
+
+        // Close the db
+        db.close();
+    });
+
+    // Declare success
+    console.dir("Called findOne!");
+});
+
