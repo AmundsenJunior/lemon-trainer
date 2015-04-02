b99f50a7814ce25f5dd570e7e9d35647cb35917f
Scott Russell <scottedwardrussell@gmail.com>
2015-04-02 11:16:55 -0400

    Added app.get to add_new_user page in app.js

--- training-steps/06_add_new_user/app.js
+++ training-steps/06_add_new_user/app.js
@@ -14,23 +14,33 @@ app.set('views', __dirname + "/views");
 var mongoclient = new MongoClient(new Server('localhost', 27017,
                                              { 'native_parser' : true }));
 var db = mongoclient.db('lemon-trainer');
-var collection = db.collection('profiles');
+
+
+// Set calls to collections
+var profiles = db.collection('profiles');
+var counters = db.collection('counters');
+var schemas = db.collection('schemas');
 
 // Open index list page of all users
 app.get('/', function (req, res) {
-    collection.find({}, {"_id" : 1, "name" : 1}).toArray(function (err, users) {
+    profiles.find({}, {"_id" : 1, "name" : 1}).toArray(function (err, users) {
         res.render('index', { users : users });
     });
 });
 
 // Open add new user page
 app.get('/users/add_new_user', function (req, res) {
-    res.render('new_user', { title : "Add a New A100 User Profile" });
+    schemas.find({"collection" : "profiles"}).toArray(function (err, fields) {
+        res.render('new_user', { 
+            title : "Add a New A100 User Profile",
+            fields : fields
+        });
+    });
 });
 
 // Open profile page for selected user
 app.get('/users/:id', function (req, res) {
-    collection.findOne({ "_id" : Number(req.params.id) }, function (err, user) {
+    profiles.findOne({ "_id" : Number(req.params.id) }, function (err, user) {
         res.render('profile', { user : user });
     });
 });
